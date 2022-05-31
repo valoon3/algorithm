d17bbbbd8ee148575d39a5476f47d21689f635e3
valoon3 <valoon2@naver.com>
2022-05-31 19:54:32 +0900

    Lv.1 신고 결과 받기

--- javascriptCoding/신고 결과 받기/ReportResult.js
+++ javascriptCoding/신고 결과 받기/ReportResult.js
@@ -5,13 +5,13 @@ function solution(id_list, report, k) {
     for(const bad of reports){
         counts.set(bad[1], counts.get(bad[1])+1 || 1);
     }
-    let good = new Map();
+    let result = new Map();
     for(const report of reports){
         if(counts.get(report[1]) >= k){
-            good.set(report[0], good.get(report[0])+1 || 1)
+            result.set(report[0], result.get(report[0])+1 || 1)
         }
     }
-    let answer = id_list.map(a => good.get(a) || 0);
+    let answer = id_list.map(a => result.get(a) || 0);
 
     return answer;
 }
