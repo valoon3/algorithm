7bb64f9480944255ee8f376e1fa6afb6b22b8afd
valoon3 <valoon2@naver.com>
2022-06-09 19:17:32 +0900

    lv2. 거리두기 확인하기 ver.2

--- javascriptCoding/lv2/거리두기 확인하기/corona.js
+++ javascriptCoding/lv2/거리두기 확인하기/corona.js
@@ -1,65 +1,85 @@
-const places =
-    [
-        ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
-        ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
-        ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
-        ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
-        ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
-    ];
+// const places =
+//     [
+//         ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+//         ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+//         ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+//         ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+//         ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
+//     ];
+//
+// const fuc = function(arr) {
+//     for(let y = 0; y <5; y++) {
+//         for(let x = 0; x < 5; x ++) {
+//             if(arr[y][x] == 'P'){
+//                 if(0<x && x<4) {
+//                     if (arr[y][x - 1] == 'P' || arr[y][x + 1] == 'P')
+//                         return 0
+//                 }
+//                 if(0<y && y<4) {
+//                     if (arr[y - 1][x] == 'P' || arr[y + 1][x] == 'P')
+//                         return 0
+//                 }
+//                 if(1<x<3) {
+//                     if (arr[y][x - 2] == 'P' && arr[y][x - 1] != 'X')
+//                         return 0
+//                     if (arr[y][x + 2] == 'P' && arr[y][x + 1] != 'X')
+//                         return 0
+//                 }
+//                 if(1<y<3) {
+//                     if (arr[y - 2][x] == 'P' && arr[y - 1][x] != 'X')
+//                         return 0
+//                     if (arr[y + 2][x] == 'P' && arr[y + 1][x] != 'X')
+//                         return 0
+//                 }
+//                 if(0<x<4 && 0<y<4) {
+//                     if (arr[y - 1][x - 1] == 'P' && arr[y - 1][x] != 'X' && arr[y][x - 1] != 'X')
+//                         return 0
+//                     if (arr[y - 1][x + 1] == 'P' && arr[y - 1][x] != 'X' && arr[y][x + 1] != 'X')
+//                         return 0
+//                     if (arr[y + 1][x - 1] == 'P' && arr[y + 1][x] != 'X' && arr[y][x - 1] != 'X')
+//                         return 0
+//                     if (arr[y + 1][x + 1] == 'P' && arr[y + 1][x] != 'X' && arr[y][x + 1] != 'X')
+//                         return 0
+//                 }
+//             }
+//         }
+//     }
+//     return 1;
+// }
+//
+// function solution(places) {
+//     var answer = [];
+//
+//     places.forEach(value => {
+//         answer.push(fuc(value));
+//     })
+//
+//
+//
+//     return answer;
+// }
+//
+// console.log(solution(places));
+//
+//
+//
 
-const fuc = function(arr) {
-    for(let y = 0; y <5; y++) {
-        for(let x = 0; x < 5; x ++) {
-            if(arr[y][x] == 'P'){
-                if(0<x && x<4) {
-                    if (arr[y][x - 1] == 'P' || arr[y][x + 1] == 'P')
-                        return 0
-                }
-                if(0<y && y<4) {
-                    if (arr[y - 1][x] == 'P' || arr[y + 1][x] == 'P')
-                        return 0
-                }
-                if(1<x<3) {
-                    if (arr[y][x - 2] == 'P' && arr[y][x - 1] != 'X')
-                        return 0
-                    if (arr[y][x + 2] == 'P' && arr[y][x + 1] != 'X')
-                        return 0
-                }
-                if(1<y<3) {
-                    if (arr[y - 2][x] == 'P' && arr[y - 1][x] != 'X')
-                        return 0
-                    if (arr[y + 2][x] == 'P' && arr[y + 1][x] != 'X')
-                        return 0
-                }
-                if(0<x<4 && 0<y<4) {
-                    if (arr[y - 1][x - 1] == 'P' && arr[y - 1][x] != 'X' && arr[y][x - 1] != 'X')
-                        return 0
-                    if (arr[y - 1][x + 1] == 'P' && arr[y - 1][x] != 'X' && arr[y][x + 1] != 'X')
-                        return 0
-                    if (arr[y + 1][x - 1] == 'P' && arr[y + 1][x] != 'X' && arr[y][x - 1] != 'X')
-                        return 0
-                    if (arr[y + 1][x + 1] == 'P' && arr[y + 1][x] != 'X' && arr[y][x + 1] != 'X')
-                        return 0
-                }
-            }
-        }
-    }
-    return 1;
-}
 
 function solution(places) {
     var answer = [];
-
-    places.forEach(value => {
-        answer.push(fuc(value));
+    answer = places.map(place => {
+        return place.some((row, rowIndex) =>
+            row.split('').some((mark, colIndex, rowArr) => {
+                if (mark === 'X') return false
+                const countPeopleAround = [
+                    rowArr[colIndex - 1] || '',
+                    rowArr[colIndex + 1] || '',
+                    (place[rowIndex - 1] || '')[colIndex],
+                    (place[rowIndex + 1] || '')[colIndex],
+                ].filter(mark => mark === 'P').length
+                return (mark === 'P' && countPeopleAround > 0) || (mark === 'O' && countPeopleAround > 1)
+            })
+        ) ? 0 : 1
     })
-
-
-
     return answer;
 }
-
-console.log(solution(places));
-
-
-
