1513515f2a7722c12041521348ee1006fddd46f3
valoon3 <valoon2@naver.com>
2022-07-07 21:29:59 +0900

    lv4. hotelroom

--- javascriptCoding/lv4/호텔 방 배정/hotelroom.js
+++ javascriptCoding/lv4/호텔 방 배정/hotelroom.js
@@ -2,18 +2,18 @@ const k = 10;
 const room_number = [1,3,4,1,3,1];
 
 function solution(k, room_number) {
-    let rooms = new Set();
+    let rooms = new Map();
     let answer = [];
 
     answer = room_number.map((value) => {
         while(rooms.has(value))
             value ++
-        rooms.add(value);
+        rooms.set(value, true);
 
         return value;
     })
 
-
+    console.log(answer);
     return answer;
 }
 
