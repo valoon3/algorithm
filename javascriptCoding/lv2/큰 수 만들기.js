f2b6d457b0870add60c18966e6b1ba8a35db5321
valoon3 <valoon2@naver.com>
2022-09-10 09:51:11 +0900

    lv2. 큰 수 만들기(수정 필요)

---
+++ javascriptCoding/lv2/큰 수 만들기.js
@@ -0,0 +1,31 @@
+
+
+function solution(number, k) {
+    var answer = '';
+    let arr = [...number];
+
+    while(k != 0) {
+        let temp = arr.slice(0, k+1);
+        let max = Math.max(...temp);
+
+        if(temp[0] == max.toString()) {
+            answer += arr.shift();
+            continue;
+        }
+        arr.shift();
+        k --;
+
+    }
+    answer += arr.join('');
+
+    return answer;
+}
+
+// solution("4177252841", 4);
+solution("1231234", 3);
+
+// 41772 52841 4 max 7
+// 7758 41 0 max 5
+
+
+
