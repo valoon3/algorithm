61f8102bda5ea90aac23ae4d0d779c006359c2c5
valoon3 <valoon2@naver.com>
2022-09-11 17:13:53 +0900

    lv2. 최솟값 만들기 // 다른사람의 풀이 포함

--- javascriptCoding/lv2/최소값 만들기/Create a minimum.js
+++ javascriptCoding/lv2/최소값 만들기/Create a minimum.js
@@ -1,22 +1,30 @@
 const [a, b] = [[1, 4, 2], [5, 4, 4]];
 
-function solution(A,B){
-    let answer = 0;
+// function solution(A,B){
+//     let answer = 0;
+//
+//     A.sort((a,b) => {
+//         return b - a;
+//     })
+//     B.sort((a,b) => {
+//         return a - b;
+//     })
+//
+//     for(let i = 0; i  < A.length; i ++) {
+//         answer += A[i] * B[i];
+//     }
+//
+//     // console.log(answer);
+//
+//     return answer;
+// }
 
-    A.sort((a,b) => {
-        return b - a;
-    })
-    B.sort((a,b) => {
-        return a - b;
-    })
-
-    for(let i = 0; i  < A.length; i ++) {
-        answer += A[i] * B[i];
-    }
-
-    // console.log(answer);
-
-    return answer;
+// 다른 사람의 풀이
+function solution(A, B) {
+    A.sort((a,b) => a-b);
+    B.sort((a,b) => b-a);
+    return A.reduce((total, value, index) => total + value * B[index], 0);
 }
 
-solution(a, b);
+;
+console.log(solution(a, b))
