372f76e8d699e2c7cb81e2a0e2a36de48747f985
valoon3 <valoon2@naver.com>
2022-10-07 22:08:11 +0900

    lv3. 최고의 집합

--- javascriptCoding/lv3/최고의 집합/best.js
+++ javascriptCoding/lv3/최고의 집합/best.js
@@ -1,8 +1,22 @@
 
 
 function solution(n, s) {
-    var answer = [];
+    if(s < n) {
+        return [-1];
+    }
 
+    let a = parseInt(s / n);
+    let b = Number(s % n);
+
+    let answer = new Array(n).fill(a);
+    for(let i = answer.length-1; i >= 0; i --) {
+        if(b == 0)
+            break;
+        answer[i] += 1;
+        b--;
+    }
 
     return answer;
 }
+
+solution(2, 9);
