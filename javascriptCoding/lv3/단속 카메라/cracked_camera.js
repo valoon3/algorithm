aa70d21710a464dc91283c99926ccac201ceac32
valoon3 <valoon2@naver.com>
2022-10-08 15:59:41 +0900

    lv3. 단속 카메라 수정

--- javascriptCoding/lv3/단속 카메라/cracked_camera.js
+++ javascriptCoding/lv3/단속 카메라/cracked_camera.js
@@ -1,29 +1,26 @@
+const routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]];
+
 function solution(routes) {
-    let answer = 0;
-    let routesArray = [];
+    let answer = 1;
+    routes.sort((a,b) => a[0] - b[0]);
+    console.log(routes);
 
-    routesArray.push(routes.shift());
+    let end = routes[0][1];
 
-    while(routes.length !== 0) {
-        let originRoute = routes.shift();
+    routes.forEach(route => {
+        let [s, e] = route;
 
-        for(let i = 0; i < routesArray.length; i ++) {
-            // 겹치는 부분이 있으면
-            if(routesArray[i][0] <= originRoute[0] <= routesArray[i][1] || routesArray[i][0] <= originRoute[1] <= routesArray[i][1]) {
-
+        if(s <= end) {
+            if(e < end) {
+                end = e;
             }
-            // 겹치는 부분이 더 크면
-            else if(originRoute[0] <= routesArray[i][0] && originRoute[1] >= routesArray[i][1]) {
-                break;
-            }
-
-            // 탐색을 다했는데 겹치는 경우가 하나도 없는경우
-            if(i == routesArray.length-1) {
-                routesArray.push(originRoute);
-            }
+        } else {
+            end = e;
+            answer ++;
         }
-
-    }
+    })
 
     return answer;
 }
+
+console.log(solution(routes));
