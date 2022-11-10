cafc61e0a8de6289aad0aaba883dcbb7a0791f0f
valoon3 <valoon2@naver.com>
2022-11-10 21:28:22 +0900

    lv.1 약수의 합

---
+++ javascriptCoding/lv1/약수의합/numSum.js
@@ -0,0 +1,14 @@
+const n = 12;
+
+function solution(n) {
+    let result = 0;
+
+    for(let i = 1; i <= n; i ++) {
+        if(n % i == 0)
+            result += i;
+    }
+
+    return result;
+}
+
+console.log(solution(n));
