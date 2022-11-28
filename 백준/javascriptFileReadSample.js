38e7170840d95fe2eb3b6d45133a7ae92f012a63
valoon3 <valoon2@naver.com>
2022-11-28 21:35:10 +0900

    백준 제출방법 샘플

--- 백준/javascriptFileReadSample.js
+++ 백준/javascriptFileReadSample.js
@@ -1,41 +1,52 @@
-// 한 줄 입력받기
-
 const readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+class BeakReadLine {
 
-let input = []
+    async readLine() {
+        return new Promise((resolve, reject) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            });
+            let input = [];
 
-rl.on("line", (line) => {
-    input = line.split(' ').map(el => parseInt(el)); // 1 2 3 4
-    rl.close();
-});
+            rl.on('line', line => {
+                input = line.split(' ').map(el => parseInt(el));
+                rl.close();
+                resolve(input);
+            })
+        });
+    }
 
-rl.on('close', () => {
-    input.forEach(el => {
-        console.log(el);
-    })
-    process.exit();
-})
+    async readManyRow(rowCount) {
+        return new Promise(async (resolve, reject) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            });
 
+            let input = [];
 
-// 여러줄 입력받기
+            let i = 0;
+            rl.on('line', line => {
+                input.push(parseInt(line));
+                i ++;
+                if(i >= rowCount) {
+                    rl.close();
+                    resolve(input);
+                }
+            });
+        });
+    }
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+}
 
-let input = []
+async function solution() {
+    let rl = new BeakReadLine();
 
-rl.on("line", (line) => {
-    input.push(line);
-});
+    // let temp = await rl.readLine();
+    let temp = await rl.readManyRow(3);
+    console.log(temp);
+}
 
-rl.on('close', () => {
-    console.log(input);
-    process.exit();
-})
+solution();
