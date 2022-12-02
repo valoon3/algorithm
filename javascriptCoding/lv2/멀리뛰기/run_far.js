fbffa8b1112cf4d8f300d51a38a2a5560c62c726
valoon3 <valoon2@naver.com>
2022-12-02 18:30:30 +0900

    lv.2 멀리 뛰기

--- javascriptCoding/lv2/멀리뛰기/run_far.js
+++ javascriptCoding/lv2/멀리뛰기/run_far.js
@@ -1,16 +1,30 @@
+// 1 = 1
+// 2 11 2 = 2
+// 3 111 12 21 3 = 3
+// 4 1111 112 121 211 31 13 = 5
+
 function solution(n) {
+    let answer = 0;
+    let workspace = ['1', '1', '2'];
 
-    if(n < 2) {
-        return 1;
-    }
-    const count = [0, 1, 2, ...Array(n-2).fill(0)];
-    count.forEach((_, i) => {
-        if(i > 2) {
-            count[i] = (count[i-2] + count[i-1]) % 1234567;
+    answer = working(n);
+
+    function working(x) {
+        if (x == 1) {
+            return 1;
+        } else if (x == 2) {
+            return 2;
+        } else {
+            if(workspace[x]) {
+                return workspace[x];
+            }
+            workspace[x] = (working(x - 1) + working(x - 2)) % 1234567;
+            return workspace[x];
         }
-    });
+    }
 
-    return count[n];
+
+    return answer;
 }
 
-console.log(solution(4));
+console.log(solution(10));
