f0684bd0f27fb4dc524e2a835401a60077feec7e
valoon3 <valoon2@naver.com>
2022-12-03 18:34:19 +0900

    lv.2 가장 큰 정사각형

---
+++ javascriptCoding/lv2/가장 큰 정사각형 찾기/mostBigSquare.js
@@ -0,0 +1,27 @@
+const board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]; // return 9
+// const board = [[0,0,1,1],[1,1,1,1]]; // return 4
+
+function solution(board)
+{
+    let answer = 0;
+    let row = board.length;
+    let col = board[0].length;
+
+    if(row < 2 || col < 2) return 1;
+
+    for(let i = 1; i < row; i ++) {
+        for(let j = 1; j < col; j ++) {
+            if(board[i][j] !== 0) {
+                let min = Math.min(board[i-1][j-1], board[i][j-1], board[i-1][j]);
+                board[i][j] = min + 1;
+            }
+
+            if(answer < board[i][j]) answer = board[i][j];
+
+        }
+    }
+
+
+
+    return answer * answer;
+}
