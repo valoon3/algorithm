410e7327080ac3138c3c98f40ec541076f94dcdb
valoon3 <valoon2@naver.com>
2022-12-09 19:31:16 +0900

    input 방법

---
+++ beakJoon/test.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('/n');
+
+const input = fs.readFileSync('./스택/').toString().split('/n');
+
+console.log(input);
