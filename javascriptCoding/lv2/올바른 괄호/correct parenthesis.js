6625d3a8ef6317c531b025eac3663795e1832876
valoon3 <valoon2@naver.com>
2023-08-19 16:29:56 +0900

    | 프로그래머스 | lv2. 올바른 괄호 복습
    1. stack

--- javascriptCoding/lv2/올바른 괄호/correct parenthesis.js
+++ javascriptCoding/lv2/올바른 괄호/correct parenthesis.js
@@ -1,20 +1,38 @@
-function solution(s){
-    let arr = [];
+// function solution(s){
+//     let arr = [];
+//
+//     for(let c  of s) {
+//         if(c == ')' && arr.length == 0){
+//             return false;
+//         }
+//         else if(c == '(') {
+//             arr.push(c);
+//         }
+//         else if(c == ')')
+//             arr.pop();
+//     }
+//
+//     if(arr.length == 0)
+//         return true;
+//     return false;
+// }
 
-    for(let c  of s) {
-        if(c == ')' && arr.length == 0){
-            return false;
-        }
-        else if(c == '(') {
-            arr.push(c);
-        }
-        else if(c == ')')
-            arr.pop();
+const s = "()()"; // result true
+// const s = "(())()"; // result true
+// const s = ")()("; // result false
+// const s = "(()("; // result false
+
+const solution = function(s) {
+    const stack = [];
+
+    if(s[0] === ')') return false;
+
+    for(let i = 0; i < s.length; i++) {
+        if(stack.length === 0 && s[i] === ')') return false
+        s[i] === '(' ? stack.push(s[i]) : stack.pop();
     }
 
-    if(arr.length == 0)
-        return true;
-    return false;
+    return stack.length === 0;
 }
 
-console.log(solution(	"()()"));
+console.log(solution(s));
