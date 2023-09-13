cbe44c68cb7d41f6126b6607894c94e3c955d387
valoon3 <valoon2@naver.com>
2023-09-13 19:45:44 +0900

    | 프로그래머스 | lv2. 점프와순간이동
    1. 시간 복잡도 수정

--- javascriptCoding/lv2/점프와순간이동/jumpAndTel.js
+++ javascriptCoding/lv2/점프와순간이동/jumpAndTel.js
@@ -4,23 +4,36 @@ const N = 5000;     // result 5
 
 function solution(n)
 {
-    let result = 1;
-    let dp = new Array(n + 1).fill(n);
-    dp[0] = 0;
+    // let result = 1;
+    // let dp = new Array(n + 1).fill(n);
+    // dp[0] = 0;
+    //
+    // for(let i = 1; i <= n; i ++) {
+    //     if(dp[i] === n) dp[i] = dp[i-1] + 1;
+    //     if(dp[i] > i) dp[i] = dp[i-1]+1;
+    //
+    //     let k = i;
+    //     while(k*2 <= n) {
+    //         dp[k * 2] = dp[i];
+    //
+    //         k *= 2;
+    //     }
+    // }
+    //
+    // return dp[dp.length-1];
 
-    for(let i = 1; i <= n; i ++) {
-        if(dp[i] === n) dp[i] = dp[i-1] + 1;
-        if(dp[i] > i) dp[i] = dp[i-1]+1;
+    let result = 0;
 
-        let k = i;
-        while(k*2 <= n) {
-            dp[k * 2] = dp[i];
-
-            k *= 2;
+    while(n > 0) {
+        if(n % 2 === 0) {
+            n /= 2;
+        } else {
+            n -= 1;
+            result ++;
         }
     }
 
-    return dp[dp.length-1];
+    return result;
 }
 
 console.log(solution(N));
