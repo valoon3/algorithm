65ff6fd4c707eaab77f72951892037c4b9489f02
valoon3 <valoon2@naver.com>
2023-10-20 17:24:10 +0900

    | 프로그래머스 | lv3. 등산코스 정하기
    1. 다익스트라 사용
    2. 최소 힙 사용으로 정렬 포함
    2. 점수 상승 => 100점

--- javascriptCoding/lv3/등산코스_정하기/chooseHikingCourse2.js
+++ javascriptCoding/lv3/등산코스_정하기/chooseHikingCourse2.js
@@ -20,7 +20,7 @@ class Node {
     }
 }
 
-class Shift {
+class Heap {
     constructor() {
         this.head = null;
         this.tail = null;
@@ -60,10 +60,77 @@ class Shift {
     }
 }
 
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    length() {
+        return this.heap.length;
+    }
+
+    swap(idx1, idx2) {
+        [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    shift() {
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const value = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return value;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        let parentIdx = Math.floor((index - 1) / 2);
+        while (
+            this.heap[parentIdx] &&
+            this.heap[index][1] < this.heap[parentIdx][1]
+            ) {
+            this.swap(index, parentIdx);
+            index = parentIdx;
+            parentIdx = Math.floor((index - 1) / 2);
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        let leftIdx = index * 2 + 1;
+        let rightIdx = index * 2 + 2;
+
+        while (
+            (this.heap[leftIdx] && this.heap[leftIdx][1] < this.heap[index][1]) ||
+            (this.heap[rightIdx] && this.heap[rightIdx][1] < this.heap[index][1])
+            ) {
+            let smallerIdx = leftIdx;
+            if (
+                this.heap[rightIdx] &&
+                this.heap[rightIdx][1] < this.heap[smallerIdx][1]
+            ) {
+                smallerIdx = rightIdx;
+            }
+
+            this.swap(index, smallerIdx);
+            index = smallerIdx;
+            leftIdx = index * 2 + 1;
+            rightIdx = index * 2 + 2;
+        }
+    }
+}
+
 function solution(n, paths, gates, summits) {
     const costDp = Array.from({length: n + 1}, () => Infinity);
     const map = Array.from({length: n + 1}, () => []);
-    const q = new Shift();
+    const q = new MinHeap();
     const visited = new Array(n+1).fill(false);
     const types = new Array(n + 1).fill('course');
     summits.sort((a,b) => a - b);
@@ -89,10 +156,9 @@ function solution(n, paths, gates, summits) {
         }
     })
 
-    console.log(map);
-    console.log('costDp : ', costDp, ' ', 'q : ', q.print());
+    // console.log('costDp : ', costDp, ' ', 'q : ', q.print());
 
-    while(!q.isEmpty()) {
+    while(q.length()) {
         const [nowPoint, intensity] = q.shift();
         // 갈 수 있는 노드가 없으면 생략
         if(!map[nowPoint].length) continue;
@@ -103,14 +169,13 @@ function solution(n, paths, gates, summits) {
             // if(cost > costDp[endPoint]) continue;
             cost = Math.max(intensity, cost);
 
-            if(cost < costDp[endPoint]) costDp[endPoint] = cost;
-
-            if(!visited[endPoint]) q.push([endPoint, cost]);
+            if(cost < costDp[endPoint]) {
+                costDp[endPoint] = cost;
+                if(!visited[endPoint]) q.push([endPoint, cost]);
+                visited[nowPoint] = true;
+            }
         }
-
-        console.log('costDp : ', costDp, ' ', 'q : ', q.print());
-
-        visited[nowPoint] = true;
+        // console.log('costDp : ', costDp, ' ', 'q : ', q.print());
     }
 
     const result = [0, Infinity];
