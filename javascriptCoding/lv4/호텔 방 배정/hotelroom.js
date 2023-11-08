b451622c4f39fdeedb425c8737fd34ef1b88fffc
valoon3 <valoon2@naver.com>
2023-11-08 17:22:36 +0900

    | 프로그래머스 | lv.4 호텔 방 배정
    1. Union find
    2. key 값에 대한 탐색이 Map 이 빠르기 때문에 Map 으로 사용해야한다.

--- javascriptCoding/lv4/호텔 방 배정/hotelroom.js
+++ javascriptCoding/lv4/호텔 방 배정/hotelroom.js
@@ -1,19 +1,28 @@
 const k = 10;
-const room_number = [1,3,4,1,3,1];
+const room_number = [1,3,4,1,3,1]; // result [1,3,4,2,5,6]
 
 function solution(k, room_number) {
-    let rooms = new Map();
+    const rooms = new Map;
     let answer = [];
 
-    answer = room_number.map((value) => {
-        while(rooms.has(value))
-            value ++
-        rooms.set(value, true);
+    const findRoom = (roomNumber) => {
+        if(!rooms.has(roomNumber)) {
+            rooms.set(roomNumber, roomNumber+1);
+            return roomNumber;
+        }
 
-        return value;
+        const nextRoom = findRoom(rooms.get(roomNumber));
+        rooms.set(roomNumber, nextRoom + 1);
+        return nextRoom;
+    }
+
+    room_number.forEach((roomNumber, index) => {
+        const roomN = findRoom(roomNumber);
+        answer.push(roomN);
     })
 
     console.log(answer);
+
     return answer;
 }
 
