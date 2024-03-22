27df701afcd9bf3dd5ef00357e0367ffc2f8cf2c
valoon3 <valoon2@naver.com>
2024-03-22 18:14:00 +0900

    | 프로그래머스 | lv.5 시험장 나누기 (진행중)
    1. 2진트리 응용
    2. 당분간 보류

--- javascriptCoding/lv5/시험장나누기/lv5_시험장나누기.js
+++ javascriptCoding/lv5/시험장나누기/lv5_시험장나누기.js
@@ -1,4 +1,5 @@
-const [k, num, links] = [3, [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1], [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]]];
+// const [k, num, links] = [3, [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1], [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]]];
+const [k, num, links] = [1, [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1], [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]]];
 // result 40
 // const [k, num, links] = [1, [6, 9, 7, 5], [[-1, -1], [-1, -1], [-1, 0], [2, 1]]];
 // result 27
@@ -21,18 +22,19 @@ class BinaryTree {
     constructor(num, links) {
         this.root = null;
         this.size = 0;
+        this.selected = new Array(num.length).fill(false); // <boolean>
 
-        const map = new Array(num.length).fill(null); // <node>
+        this.map = new Array(num.length).fill(null); // <node>
 
         num.forEach((v, i) => {
-            map[i] = new Node(i, v);
+            this.map[i] = new Node(i, v);
             this.size ++;
         })
 
         links.forEach(([left, right], i) => {
-            const node = map[i];
-            const leftNode = left !== -1 ? map[left] : null;
-            const rightNode = right !== -1 ? map[right] : null;
+            const node = this.map[i];
+            const leftNode = left !== -1 ? this.map[left] : null;
+            const rightNode = right !== -1 ? this.map[right] : null;
 
             if(leftNode) {
                 node.left = leftNode
@@ -45,7 +47,8 @@ class BinaryTree {
             }
         })
 
-        this.root = map[0];
+        this.root = this.map[0];
+
         while(this.root.parent) {
             this.root = this.root.parent;
         }
@@ -70,30 +73,25 @@ class BinaryTree {
     }
 
     result(k) {
-        if(k === 1) return this.root.value;
-
-
+        if (k === 1) return this.root.value;
 
 
     }
 
-    cutTree(node, k) {
-        if(k === 1) return node;
+    addSelect(k) {
+        if(k === 0) return;
+        let node = this.root;
 
-        if(k === 2) {
-            if(node.left && node.right) {
-            } else if(node.left) {
-                return node.left.value;
-            } else if(node.right) {
-                return node.right.value;
+        while(!this.selected[node.name]) {
+            if(node.left && !this.selected[node.left.name]) {
+                node = node.left;
+            } else if(node.right && !this.selected[node.right.name]) {
+                node = node.right;
             }
-        } else {
-
         }
-    }
 
-    absolute(value1, value2) {
-        return Math.abs(value1 - value2);
+        this.selected[node.name] = true;
+        this.addSelect(k-1);
     }
 
 
@@ -104,7 +102,7 @@ class BinaryTree {
 
 function solution(k, num, links) {
     const tree = new BinaryTree(num, links);
-    console.log(tree);
+    return tree.result(k);
 
 }
 
