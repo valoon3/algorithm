f6147bc711d0f2ce8f423962e7ab93bf4758a723
valoon3 <valoon2@naver.com>
2024-03-23 16:21:43 +0900

    | 프로그래머스 | lv.3 외벽 점검
    1. 그리디로 풀었다
    2. 96.1 나중에 다시 시도
    3. 다른 풀이로도 풀어보자 (백트레킹)

--- javascriptCoding/lv3/외벽_점검/wall_inspection3.js
+++ javascriptCoding/lv3/외벽_점검/wall_inspection3.js
@@ -1,49 +1,66 @@
-const [n, weak, dist] =
-    [12, [1, 5, 6, 10], [1, 2, 3, 4]];
+// const [n, weak, dist] = [12, [1, 5, 6, 10], [1, 2, 3, 4]];
 // result 2
 
 // const [n, weak, dist] = [12, [1, 3, 4, 9, 10]	, [3, 5, 7]	];
 // result 1
 
 // const [n, weak, dist] = [200, [0, 100], [1,1]]
+const [n, weak, dist] = [12, [10, 0], [1,2]]; // 1
 
 // n : 벽의 길이
 // weak : 취약 지점
 // dist : 친구들의 이동 가능 거리
 
 function solution(n, weak, dist) {
-    let answer = -1;
-    let memberCount = dist.length;
+    let answer = Infinity;
+    const friendCount = dist.length;
+    dist.sort((a,b) => b - a);
 
-    dist.sort((a, b) => a - b);
+    const len = n;
 
-    function fix(weak, startPoint, friend) {
-        const over = startPoint + friend >= n;
-        return over ?
-            weak.filter(w => w < startPoint && w > startPoint + friend - n) :
-            weak.filter(w => w < startPoint || w > startPoint + friend);
+    const fixWeak = (weak, start, friend) => {
+        const end = start + friend;
+
+        if(end <= len) {
+            const newWeak = [];
+            weak.forEach(v => {
+                if(v >= start && v <= end) {
+
+                } else {
+                    newWeak.push(v);
+                }
+            })
+
+            return newWeak;
+        } else {
+            const newWeak = [];
+            weak.forEach(v => {
+                if((v >= start && v <= len)) {}
+                else if((0 <= v && v <= end - len)) {}
+                else newWeak.push(v);
+            })
+            return newWeak;
+        }
     }
 
-    const weakQueue = [[weak, dist.length-1]];
-    while(weakQueue.length) {
-        const [weak, friendIndex] = weakQueue.shift();
-        const friend = dist[friendIndex];
-
-        for(let startPoint of weak) {
-            const newWeakArr = fix(weak, startPoint, friend);
-            if(newWeakArr.length === 0) {
-                answer = memberCount - friendIndex;
-                break;
-            }
-            weakQueue.push([newWeakArr, friendIndex-1]);
+    const dfs = (weak, friendNumber) => {
+        if(weak.length === 0) {
+            answer = Math.min(answer, friendNumber);
+            return;
         }
 
-        console.log(weakQueue);
+        const friend = dist[friendNumber];
 
-        if(answer !== -1) break;
+        if(friendNumber < friendCount) {
+            weak.forEach(start => {
+                const newWeak = fixWeak(weak, start, friend);
+                dfs(newWeak, friendNumber + 1);
+            })
+        }
     }
 
-    return answer;
+    dfs(weak, 0);
+    return answer === Infinity ? -1 : answer;
 }
 
 console.log(solution(n, weak, dist));
